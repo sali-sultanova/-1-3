2db0b176b41211f48d2435c5f167e0324da82ec7
sali-sultanova <marpl.d@yandex.ru>
2026-03-01 20:32:35 +0300

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -10,14 +10,14 @@ min1 = nums[0]
 for i in range(1,10):
     if nums[i] < min1:
         min1 = nums[i]
-sum = 0
+sum1 = 0
 
 even = []
 for i in range(10):
-    sum += nums[i]
+    sum1 += nums[i]
     if nums[i] % 2 == 0:
         even.append(nums[i])
-ave = sum / 10
+ave = sum1 / 10
 print("Список чисел:", nums)
 print("Максимальное число:", max1)
 print("Минимальное число:", min1)
